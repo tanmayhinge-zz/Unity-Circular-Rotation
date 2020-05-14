3f4e9572c1d94835c0ad12469740b43ca8a9f6ec
Tanmay Hinge <hingetansu18ce@student.mes.ac.in>
2020-05-15 04:13:26 +0530

    Create CircularMovement.cs

---
+++ CircularMovement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircularMovement : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform rotationCenter;
+
+    public float moveSpeed = 4.0f;
+    [SerializeField]
+    private bool moveDown;
+
+    [SerializeField]
+    float rotationRadius = 2f, angularSpeed = 2f;
+
+    float posX, posY, angle = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        posX = rotationCenter.position.x + Mathf.Cos(angle) * rotationRadius;
+        posY = rotationCenter.position.y + Mathf.Sin(angle) * rotationRadius;
+        transform.position = new Vector2(posX, posY);
+        angle = angle + Time.deltaTime * angularSpeed;
+
+        if (angle >= 360f)
+            angle = 0f;
+    }
+    
+    //below this line can be neglected 
+    void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.tag == "Bounce" || target.tag == "Enemy")
+        {
+            moveDown = !moveDown;
+        }
+
+    }
+}
